2d02178e05f8c125b6848d30bc43a958823e4520
unaay20 <junml.kimmich@gmail.com>
2026-01-02 14:02:25 -0600

    removed cors 2

--- services/schedule-service/src/app.ts
+++ services/schedule-service/src/app.ts
@@ -1,10 +1,8 @@
 import "dotenv/config";
 import express from "express";
-import cors from "cors";
 import scheduleRoutes from "./routes/schedule.routes";
 
 const app = express();
-app.use(cors());
 app.use(express.json());
 
 app.use("/schedule", scheduleRoutes);
