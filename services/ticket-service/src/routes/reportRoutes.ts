9be483464765345bb2153caa279ff8832fdeabf2
unaay20 <junml.kimmich@gmail.com>
2026-01-05 12:11:51 -0600

    auth middleware added to all services}

--- services/ticket-service/src/routes/reportRoutes.ts
+++ services/ticket-service/src/routes/reportRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import * as ReportController from "../controllers/reportController";
+const { Authorize } = require ("@sidaroco/auth_middleware");
 
 const router = Router();
 
-router.get("/tickets/daily", ReportController.getDailyTicketReport);
+router.get("/tickets/daily", Authorize(), ReportController.getDailyTicketReport);
 router.get("/earnings/monthly", ReportController.getMonthlyEarningsReport);
 router.get("/earnings/monthly/by-route",ReportController.getMonthlyRouteReport);
 
