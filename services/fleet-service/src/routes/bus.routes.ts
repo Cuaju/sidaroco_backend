b00b29c9598cca39aff3f53834f8e7f56ca9c4f9
unaay20 <junml.kimmich@gmail.com>
2026-01-05 12:49:22 -0600

    start auth middleware

--- services/fleet-service/src/routes/bus.routes.ts
+++ services/fleet-service/src/routes/bus.routes.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import multer from "multer";
 import * as BusController from "../controllers/bus.controller";
+const { Authorize } = require("@sidaroco/auth_middleware");
 
 const router = Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
 router.post("/",  upload.single("photo"), BusController.createBus);
 router.get("/:id",  BusController.getBusById);
-router.get("/",  BusController.getAllBuses);
+router.get("/",  Authorize({roles:["RouteManager"]}), BusController.getAllBuses);
 router.put("/:id",  upload.single("photo"), BusController.updateBus);
 router.delete("/:id", BusController.deleteBus);
 
